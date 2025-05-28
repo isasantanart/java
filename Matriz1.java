0994344dceff9b9b4079bd5e6ccd0c6e5223e4a6
isasantanart <isabelaleandro1a@gmail.com>
2025-05-28 08:58:12 -0300

    matriz 5x5

--- Matriz1.java
+++ Matriz1.java
@@ -1,40 +1,33 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+1. Crie um programa que leia uma matriz 5x5. Em seguida, conte quantos números pares
+existem na matriz. :)
 
 *******************************************************************************/
 import java.util.Scanner;
-public class Matriz1
-{
-    public static void main(String[] args) {
-    //criando matriz 3x3 vazia     
-    int [][] matriz = new int [3][3];
-    
-    //Atribuindo valores manualmente 
-    matriz[0][0] = 5;
-    matriz[0][1] = 3;
-    matriz[0][2] = 2;
-    
-    matriz[1][0] = 4;
-    matriz[1][1] = 3;
-    matriz[1][2] = 6;
-    
-    matriz[2][0] = 7;
-    matriz[2][1] = 8;
-    matriz[2][2] = 1;
-    
-    // Imprimindo a matriz 
-    System.out.println("Matriz 3x3:");
-    for (int i = 0; i < matriz.length; i++) { //tamanho da matriz principal (linha) matriz.length 
-         for (int j = 0; j < matriz[i].length; j++) { //tamanho da matriz (coluna) matriz[i].length 
-             System.out.print (matriz [i][j] + " "); //saida da matriz (matriz [i][j])
-             
-         }     
-        System.out.println(); //quebra de linha apos cada linha da matriz 
-    
-    }
-}
+public class Matriz1 {
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+		int [][] matriz = new int [5][5];
+		int contadorPares = 0;
+		
+		System.out.println("Digite os números da matriz:");
+		
+		
+		for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("Número [%d][%d]: ", i, j);
+                matriz[i][j] = ler.nextInt();
+            }
+		}
+		
+		for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (matriz[i][j] % 2 == 0) {
+                    contadorPares++;
+                }
+            }
+        }
+        System.out.println("Quantidade de números pares: " + contadorPares);
+	}
 }
